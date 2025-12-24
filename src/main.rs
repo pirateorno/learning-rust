2a77388da75e0cbdb840ec4e1bd71197564366a0
Ya_Sanya__ <sasaforostovskij@gmail.com>
2025-12-25 01:58:33 +0200

    testing github cli

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Salam zaebal!");
+    println!("Salam zaebal");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
